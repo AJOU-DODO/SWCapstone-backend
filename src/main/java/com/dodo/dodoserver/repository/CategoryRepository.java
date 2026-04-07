a53a57eb9a340d4f95707d871060aee54fd934cd
gnsehs <gks9315@naver.com>
2026-04-07 23:04:12 +0900

    feat/#9: 카테고리 엔티티 및 리포지토리 추가

---
+++ src/main/java/com/dodo/dodoserver/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.dodo.dodoserver.repository;
+
+import com.dodo.dodoserver.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
