e537112ea12e33c1eb2b6249d46ad8590fbde2e1
gnsehs <gks9315@naver.com>
2026-04-10 01:22:17 +0900

    feat/#9: fcm token 로직 추가

---
+++ src/main/java/com/dodo/dodoserver/dto/DeviceRequestDto.java
@@ -0,0 +1,21 @@
+package com.dodo.dodoserver.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 기기 등록 및 FCM 토큰 전송을 위한 DTO입니다.
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class DeviceRequestDto {
+
+    @NotBlank(message = "FCM 토큰은 필수입니다.")
+    private String fcmToken;
+
+    private String deviceType; // 예: "ANDROID", "IOS"
+}
