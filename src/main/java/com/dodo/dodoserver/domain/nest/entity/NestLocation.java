addf559912eb22b9368b71972997973eaa5581c1
gnsehs <gks9315@naver.com>
2026-04-16 16:56:40 +0900

    refactor/#17: 위경도 정보 Spatial Point 사용

--- src/main/java/com/dodo/dodoserver/domain/nest/entity/NestLocation.java
+++ src/main/java/com/dodo/dodoserver/domain/nest/entity/NestLocation.java
@@ -2,6 +2,7 @@ package com.dodo.dodoserver.domain.nest.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -25,13 +26,24 @@ public class NestLocation {
     @JoinColumn(name = "nest_id")
     private Nest nest;
 
-    @Column(nullable = false, precision = 18, scale = 10)
-    private Double latitude;
-
-    @Column(nullable = false, precision = 18, scale = 10)
-    private Double longitude;
+    /**
+     * SRID 4326: GPS 좌표계 (WGS84)
+     * POINT(longitude, latitude) 순서로 저장됨에 주의
+     */
+    @Column(nullable = false, columnDefinition = "POINT SRID 4326")
+    private Point point;
 
     @CreatedDate
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+    
+    // 편의 메서드: 위도 추출
+    public Double getLatitude() {
+        return point != null ? point.getY() : null;
+    }
+
+    // 편의 메서드: 경도 추출
+    public Double getLongitude() {
+        return point != null ? point.getX() : null;
+    }
 }
