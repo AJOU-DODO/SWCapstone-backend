41a70b747fb983852d7604200fcb85529ec786f4
gnsehs <gks9315@naver.com>
2026-04-16 17:11:17 +0900

    feat/#17: NestSummaryResponseDto에 isUnlocked 추가

--- src/main/java/com/dodo/dodoserver/domain/nest/dto/NestSummaryResponseDto.java
+++ src/main/java/com/dodo/dodoserver/domain/nest/dto/NestSummaryResponseDto.java
@@ -13,14 +13,16 @@ public class NestSummaryResponseDto {
     private String title;
     private String thumbnailUrl;
     private boolean isAd;
+    private boolean isUnlocked;
 
-    public static NestSummaryResponseDto from(Nest nest) {
+    public static NestSummaryResponseDto from(Nest nest, boolean isUnlocked) {
         String thumbnail = nest.getImages().isEmpty() ? null : nest.getImages().get(0).getImageUrl();
         return NestSummaryResponseDto.builder()
                 .id(nest.getId())
                 .title(nest.getTitle())
                 .thumbnailUrl(thumbnail)
                 .isAd(nest.isAd())
+                .isUnlocked(isUnlocked)
                 .build();
     }
 }
