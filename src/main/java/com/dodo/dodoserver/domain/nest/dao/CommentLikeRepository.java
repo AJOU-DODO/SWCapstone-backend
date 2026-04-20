88f915b8949609352ff6ffa9d31817cb51ae122f
gnsehs <gks9315@naver.com>
2026-04-20 17:40:34 +0900

    feat/#17: 댓글 좋아요 entity dao

---
+++ src/main/java/com/dodo/dodoserver/domain/nest/dao/CommentLikeRepository.java
@@ -0,0 +1,13 @@
+package com.dodo.dodoserver.domain.nest.dao;
+
+import com.dodo.dodoserver.domain.nest.entity.CommentLike;
+import com.dodo.dodoserver.domain.nest.entity.NestComment;
+import com.dodo.dodoserver.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+    Optional<CommentLike> findByUserAndComment(User user, NestComment comment);
+    boolean existsByUserAndComment(User user, NestComment comment);
+}
---
+++ src/main/java/com/dodo/dodoserver/domain/nest/entity/CommentLike.java
@@ -0,0 +1,38 @@
+package com.dodo.dodoserver.domain.nest.entity;
+
+import com.dodo.dodoserver.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "comment_likes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"comment_id", "user_id"})
+})
+@EntityListeners(AuditingEntityListener.class)
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private NestComment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
