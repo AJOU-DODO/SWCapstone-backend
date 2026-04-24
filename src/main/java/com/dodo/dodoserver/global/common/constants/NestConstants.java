947de5bdb41459cfb2866aa789c662e4fd17ad82
gnsehs <gks9315@naver.com>
2026-04-25 00:17:39 +0900

    chore/#35: 기본 둥지 탐색 거리 상수화

---
+++ src/main/java/com/dodo/dodoserver/global/common/constants/NestConstants.java
@@ -0,0 +1,9 @@
+package com.dodo.dodoserver.global.common.constants;
+
+public final class NestConstants {
+
+	public static final double DEFAULT_FIND_RADIUS_METER = 2000.0;
+
+	private NestConstants() {
+	}
+}
