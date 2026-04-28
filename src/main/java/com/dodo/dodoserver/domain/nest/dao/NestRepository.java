34cb1af084d1fd8df5c82e12fd1d15a11d9e87fc
gnsehs <gks9315@naver.com>
2026-04-28 15:54:09 +0900

    feat/#22: 스케줄링 활성화 및 NestRepository 업데이트

--- src/main/java/com/dodo/dodoserver/domain/nest/dao/NestRepository.java
+++ src/main/java/com/dodo/dodoserver/domain/nest/dao/NestRepository.java
@@ -3,6 +3,13 @@ package com.dodo.dodoserver.domain.nest.dao;
 import com.dodo.dodoserver.domain.nest.dao.querydsl.NestRepositoryCustom;
 import com.dodo.dodoserver.domain.nest.entity.Nest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NestRepository extends JpaRepository<Nest, Long>, NestRepositoryCustom {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Nest n SET n.viewCount = n.viewCount + :increment WHERE n.id = :nestId")
+    void incrementViewCount(@Param("nestId") Long nestId, @Param("increment") Long increment);
 }
