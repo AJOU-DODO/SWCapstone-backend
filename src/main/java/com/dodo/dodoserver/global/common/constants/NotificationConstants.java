d900fbbf7fe2ef7d295697490cc5fe5f99ad7646
gnsehs <gks9315@naver.com>
2026-04-28 14:12:40 +0900

    feat/#38: 좋아요 알림 상수 추가

--- src/main/java/com/dodo/dodoserver/global/common/constants/NotificationConstants.java
+++ src/main/java/com/dodo/dodoserver/global/common/constants/NotificationConstants.java
@@ -11,12 +11,18 @@ public final class NotificationConstants {
     // Type Values
     public static final String TYPE_COMMENT = "COMMENT";
     public static final String TYPE_REPLY = "REPLY";
+    public static final String TYPE_NEST_LIKE = "NEST_LIKE";
+    public static final String TYPE_COMMENT_LIKE = "COMMENT_LIKE";
 
     // Message Templates
     public static final String TITLE_NEW_COMMENT = "둥지에 새 댓글이 달렸습니다!";
     public static final String BODY_NEW_COMMENT = "%s님이 댓글을 남겼습니다.";
     public static final String TITLE_NEW_REPLY = "내 댓글에 답글이 달렸습니다!";
     public static final String BODY_NEW_REPLY = "%s님이 답글을 남겼습니다.";
+    public static final String TITLE_NEST_LIKE = "내 둥지에 좋아요가 달렸어요!";
+    public static final String BODY_NEST_LIKE = "%s님이 회원님의 둥지를 좋아합니다.";
+    public static final String TITLE_COMMENT_LIKE = "내 댓글에 좋아요가 달렸어요!";
+    public static final String BODY_COMMENT_LIKE = "%s님이 회원님의 댓글을 좋아합니다.";
 
     private NotificationConstants() {
     }
