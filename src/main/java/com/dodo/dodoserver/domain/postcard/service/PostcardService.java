9e6e802bb093726b96efd649ab3924b41190cae9
gnsehs <gks9315@naver.com>
2026-05-05 14:15:21 +0900

    fix/#41: postcard nullPointException 수정

--- src/main/java/com/dodo/dodoserver/domain/postcard/service/PostcardService.java
+++ src/main/java/com/dodo/dodoserver/domain/postcard/service/PostcardService.java
@@ -155,7 +155,7 @@ public class PostcardService {
         Postcard postcard = postcardRepository.findById(postcardId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.POSTCARD_NOT_FOUND));
 
-        if (!postcard.getCurrentOwner().getId().equals(userId)) {
+        if (postcard.getCurrentOwner() == null || !postcard.getCurrentOwner().getId().equals(userId)) {
             throw new BusinessException(ErrorCode.NOT_POSTCARD_OWNER);
         }
 
