e650c0edb9186e41062e53688375471bb292caa8
gnsehs <gks9315@naver.com>
2026-05-13 16:28:51 +0900

    feat/#49: 공지사항 레포지토리 및 관리자용 Querydsl 구현

---
+++ src/main/java/com/dodo/dodoserver/domain/notice/dao/NoticeRepository.java
@@ -0,0 +1,13 @@
+package com.dodo.dodoserver.domain.notice.dao;
+
+import com.dodo.dodoserver.domain.notice.entity.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    // 사용자용 레포지토리 - @SQLRestriction("deleted_at IS NULL") 자동 적용됨
+    Optional<Notice> findByIdAndIsPublishedTrue(Long id);
+}
