f91dcb8cadb68653c10dd5c49d33ed70634edaa7
gnsehs <gks9315@naver.com>
2026-05-17 15:31:35 +0900

    !HOTFIX: postcard inventory 필터 수정안 테스트 코드

--- src/test/java/com/dodo/dodoserver/domain/postcard/service/PostcardServiceTest.java
+++ src/test/java/com/dodo/dodoserver/domain/postcard/service/PostcardServiceTest.java
@@ -211,6 +211,44 @@ class PostcardServiceTest {
     }
 
     @Test
+    @DisplayName("엽서 인벤토리 조회 성공 - 내가 생성하고 아직 공유 안한 엽서만")
+    void getPostcardInventory_createdNotShared_success() {
+        // given
+        List<Postcard> createdList = List.of(myPostcard);
+        Page<Postcard> inventoryPage = new PageImpl<>(createdList);
+        given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
+        given(postcardRepository.findCreatedNotSharedByUser(eq(user), any(Pageable.class))).willReturn(inventoryPage);
+
+        // when
+        Page<PostcardResponseDto> result = postcardService.getPostcardInventory(user.getId(), "CREATED_NOT_SHARED", Pageable.unpaged());
+
+        // then
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent().get(0).getId()).isEqualTo(myPostcard.getId());
+        assertThat(result.getContent().get(0).isShared()).isFalse();
+    }
+
+    @Test
+    @DisplayName("엽서 인벤토리 조회 성공 - 내가 생성하고 둥지에 공유한 엽서만")
+    void getPostcardInventory_createdShared_success() {
+        // given
+        myPostcard.setShared(true);
+        myPostcard.setCurrentOwner(null);
+        List<Postcard> sharedList = List.of(myPostcard);
+        Page<Postcard> inventoryPage = new PageImpl<>(sharedList);
+        given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
+        given(postcardRepository.findCreatedSharedByUser(eq(user), any(Pageable.class))).willReturn(inventoryPage);
+
+        // when
+        Page<PostcardResponseDto> result = postcardService.getPostcardInventory(user.getId(), "CREATED_SHARED", Pageable.unpaged());
+
+        // then
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent().get(0).getId()).isEqualTo(myPostcard.getId());
+        assertThat(result.getContent().get(0).isShared()).isTrue();
+    }
+
+    @Test
     @DisplayName("엽서 인벤토리 조회 성공 - 내가 가져온 엽서만")
     void getPostcardInventory_acquired_success() {
         // given
