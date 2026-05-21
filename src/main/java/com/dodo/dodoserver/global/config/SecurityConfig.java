2038b3418574a9ebf4294986cceb3ea8ebcc7a1b
gnsehs <gks9315@naver.com>
2026-05-21 19:51:19 +0900

    refactor/#57: OAuth2RedirectUriFilter 배치를 OAuth2AuthorizationRequestRedirectFilter 이전으로 조정

--- src/main/java/com/dodo/dodoserver/global/config/SecurityConfig.java
+++ src/main/java/com/dodo/dodoserver/global/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.security.web.session.DisableEncodeUrlFilter;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -79,7 +79,7 @@ public class SecurityConfig {
 				.successHandler(oauth2AuthenticationSuccessHandler)
 			)
 
-			.addFilterBefore(oauth2RedirectUriFilter, DisableEncodeUrlFilter.class)
+			.addFilterBefore(oauth2RedirectUriFilter, OAuth2AuthorizationRequestRedirectFilter.class)
 			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
